809e9b46344e9109a59a82a0937448cd3a31012b
adorahelen <adorahelenmin@gmail.com>
2024-06-23 23:59:05 +0900

    yet

---
+++ test0623.py
@@ -0,0 +1,45 @@
+# 두 수의 차
+# 문제 설명
+# 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+# def solution(num1, num2):
+#     return num1 - num2
+
+
+# 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+
+# def solution(num1, num2):
+    
+#     return num1 // num2
+
+#     *파이썬 나누기 /은 소수를 구할 때, //는 정수만 구할 때
+
+
+# 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+# def solution(num1, num2):
+#     if num1 == num2:
+#         return 1
+#     else:
+#         return -1
+
+# 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+# def solution(num1, num2):
+#     answer = num1 % num2
+#     return answer
+
+
+# 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+# def solution(age):
+#     answer = 2022 - age
+#     return answer+1
+
+# 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+# def solution(num1, num2):
+#     answer = num1 + num2
+#     return answer
+
+# 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+# def solution(num1, num2):
+#     answer = num1 * num2
+#     return answer
+
