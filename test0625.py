6620017619cc2239715ba24369b5acf22e1093fe
adorahelen <adorahelenmin@gmail.com>
2024-06-25 10:44:23 +0900

    loof loof

---
+++ test0625.py
@@ -0,0 +1,17 @@
+# 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+# def solution(num1, num2):
+#     answer = num1 / num2 *1000
+#     return int(answer)
+
+# 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+# def solution(n):
+    
+#     answer = 0
+#     i = 0
+#     while i <= n: 
+#         if i % 2 == 0:
+#             answer += i
+#         i += 1
+            
+#     return answer
+
